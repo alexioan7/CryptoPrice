f4e2780d0b2f78314f3498be7c145a27a739bfc2
Alexandros Ioannou <alexioan92@gmail.com>
2021-08-20 12:58:29 +0300

    first commit

---
+++ app/src/main/java/com/alexandros/cryptoprice/Adapter.java
@@ -0,0 +1,32 @@
+package com.alexandros.cryptoprice;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    @NonNull
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder  extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
